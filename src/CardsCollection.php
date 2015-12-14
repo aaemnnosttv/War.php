88dc124be2103b00417a8fafd71029bc867a8b8a
Evan Mattson <me@aaemnnost.tv>
2015-12-13 23:54:07 -0500

    add collection helper method

--- src/CardsCollection.php
+++ src/CardsCollection.php
@@ -11,6 +11,11 @@ class CardsCollection extends Collection
         $this->items = [];
     }
 
+    public function assimilate(Collection $collection)
+    {
+        $this->items = $this->merge($collection)->all();
+    }
+
     public static function from(... $cards)
     {
         return new static($cards);
